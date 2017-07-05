ebacf79b8b419bdd487f421d377ff64f8d15e12e
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-05 19:05:56 -0400

    Recipe item added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent} from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
 
   ],
   imports: [
