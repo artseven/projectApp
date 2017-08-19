209eade74fb8d51aa4c1d1ec5281fb9d6687f3e9
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-08-19 16:27:56 -0400

    Output

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @Output() ingredient: Ingredient;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onButtonSubmit() {
+    console.log('BUTTON SUBMITTED');
   }
 
 }
