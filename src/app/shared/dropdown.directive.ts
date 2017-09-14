6286a6cfeb85aa4b5d5e32f5712a68e4a2a090fc
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-14 17:16:54 -0400

    HostBinding to toggle class

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,9 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]'
 })
 
 export class DropdownDirective {
-    
+
+    isOpen= false;
+
+    @HostListener('click').toggleOpen() {
+        this.isOpen = !this.isOpen;
+    }
 }
